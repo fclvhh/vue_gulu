66ecaa73e093ec0d779b53b490d123beaea4e70e
fcdada <374822251@qq.com>
2021-10-28 16:13:04 +0800

    利用parcel，将button.js 文件重构成 单文件组件 1.> 依赖集中到了入口文件 app.js 2.> 支持导入导出语法

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+import button from "./button";
+import Vue from "vue";
+Vue.component('g-button',button)
+new Vue({
+  "el":'#app',
+  "data":{
+    "message": '只有我一个人嘛？'
+  }
+})
---
+++ src/button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button @click="handleClick" class="g-button">点我</button>
+</template>
+<script>
+ export default {
+   data() {
+     return {
+       xxx:''
+     }
+   },
+   methods:{
+     handleClick() {
+       console.log('click true!')
+     }
+   }
+ }
+</script>
+<style lang="scss">
+.g-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+}
+
+.g-button:hover {
+  border-color: var(--border-color-hover);
+}
+
+.g-button:active {
+  background-color: var(--button-active-bg);
+}
+
+.g-button:focus {
+  outline: none;
+}
+</style>
