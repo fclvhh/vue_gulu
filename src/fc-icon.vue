b5be877f0620901b2633d4947e8db130f51c5c76
fcdada <374822251@qq.com>
2022-04-11 11:27:57 +0800

    利用分支writeButton熟悉Vue

---
+++ src/fc-icon.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg aria-hidden="true" class="icon" :class="{loading:loadState}">
+    <use xlink:href="#i-settings"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "fc-icon",
+  data() {
+    return {
+      loadState: this.loadState
+    }
+  },
+  props:["loadState"],
+  mounted() {
+    console.log(this.loadState)
+  }
+
+}
+</script>
+
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  order: 0;
+  margin-right: .5rem;
+}
+.loading {
+  animation: spinMy 1s linear infinite;
+}
+@keyframes spinMy {
+    from {
+      transform: rotate(0deg);
+    }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
